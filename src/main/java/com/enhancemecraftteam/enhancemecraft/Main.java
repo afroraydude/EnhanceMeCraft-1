5fe5626e916f82d05ffc2e65d8efcfe6757c890f
afroraydude <rayjo5000@yahoo.com>
2014-09-22 13:32:46 -0400

    Create Main.java

---
+++ src/main/java/com/enhancemecraftteam/enhancemecraft/Main.java
@@ -0,0 +1,54 @@
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeManager;
+
+import javax.sound.sampled.CompoundControl;
+
+import static net.minecraftforge.common.BiomeDictionary.registerBiomeType;
+import static net.minecraftforge.common.BiomeManager.BiomeEntry;
+import static net.minecraftforge.common.BiomeManager.addSpawnBiome;
+import static net.minecraftforge.common.BiomeManager.addVillageBiome;
+
+/**
+ * Created by EnhanceMeCraftTeam.
+ * For minecraft version 1.7.10 and Forge build 1208 or higher
+ */
+@Mod(name="enhancemecraftmod", modid="enhancemecraft", version="indev")
+public class main
+{
+
+      @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+
+      
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+
+      
+
+    }
+  
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e)
+    {
+    
+      
+    
+    }
+
+}
